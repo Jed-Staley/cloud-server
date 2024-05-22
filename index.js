6b6b0d0bf25b7e466ce17353026359c0b66c18c2
Jed Staley <JNStaley1@gmail.com>
2024-05-22 19:01:12 -0400

    Updatetd for database integration

--- index.js
+++ index.js
@@ -2,16 +2,31 @@
 
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+const itemRoutes = require('./routes/items');
 
 const app = express();
 app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Could not connect to the database', err))
+;
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World');
 });
 
+app.use('/items', itemRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}`);
 })
